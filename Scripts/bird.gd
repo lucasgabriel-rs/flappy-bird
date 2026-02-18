db2e86442a8f6a4e3bfd381a5dec85dc5fea9803
Lucas Ramos <lucasgabrielramosdasilva94@gmail.com>
2026-02-18 04:08:25 -0300

    Physics

--- Scripts/bird.gd
+++ Scripts/bird.gd
@@ -4,22 +4,29 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var didStartGame: bool = false
+var isDead: bool = false
+
 
 func _physics_process(delta: float) -> void:
+	if get_slide_collision_count() != 0:
+		die()
+	
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and didStartGame:
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and not isDead:
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		if not didStartGame:
+			didStartGame = true
+	
 	move_and_slide()
+
+
+func die() -> void:
+	if not isDead:
+		isDead = true
+		print("Morreu!")
+	
---
+++ Scripts/main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+@onready var tap_tap: TextureRect = $TapTap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_touch_screen_button_pressed() -> void:
+	tap_tap.visible = false
